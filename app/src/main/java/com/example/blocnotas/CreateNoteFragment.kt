f1777fc0a645ad30126abe61b59160a6fae375b4
BryanParamo <s19120180@alumnos.itsur.edu.mx>
2023-10-23 01:49:34 -0600

    Implementaciones mamalonas 2 (parchando bugs pa)

--- app/src/main/java/com/example/blocnotas/CreateNoteFragment.kt
+++ app/src/main/java/com/example/blocnotas/CreateNoteFragment.kt
@@ -1,6 +1,5 @@
 package com.example.blocnotas
 
-
 import android.Manifest
 import android.app.Activity
 import android.app.Activity.RESULT_OK
@@ -192,7 +191,41 @@ class CreateNoteFragment : BaseFragment(),EasyPermissions.PermissionCallbacks,Ea
         }
 
         else{
-            @@ -266,7 +266,7 @@ class CreateNoteFragment : BaseFragment(),EasyPermissions.PermissionCallbacks,Ea
+            launch {
+                var notes = Notes()
+                notes.title = etNoteTitle.text.toString()
+                notes.subTitle = etNoteSubTitle.text.toString()
+                notes.noteText = etNoteDesc.text.toString()
+                notes.dateTime = currentDate
+                notes.color = selectedColor
+                notes.imgPath = selectedImagePath
+                notes.webLink = webLink
+                context?.let {
+                    NotesDatabase.getDatabase(it).noteDao().insertNotes(notes)
+                    etNoteTitle.setText("")
+                    etNoteSubTitle.setText("")
+                    etNoteDesc.setText("")
+                    layoutImage.visibility = View.GONE
+                    imgNote.visibility = View.GONE
+                    tvWebLink.visibility = View.GONE
+                    requireActivity().supportFragmentManager.popBackStack()
+                }
+            }
+        }
+    }
+    private fun deleteNote(){
+        launch {
+            context?.let {
+                NotesDatabase.getDatabase(it).noteDao().deleteSpecificNote(noteId)
+                requireActivity().supportFragmentManager.popBackStack()
+            }
+        }
+    }
+    private fun checkWebUrl(){
+        if (Patterns.WEB_URL.matcher(etWebLink.text.toString()).matches()){
+            layoutWebUrl.visibility = View.GONE
+            etWebLink.isEnabled = false
+            webLink = etWebLink.text.toString()
             tvWebLink.visibility = View.VISIBLE
             tvWebLink.text = etWebLink.text.toString()
         }else{
@@ -200,3 +233,132 @@ class CreateNoteFragment : BaseFragment(),EasyPermissions.PermissionCallbacks,Ea
             Toast.makeText(requireContext(),"Url no es valido",Toast.LENGTH_SHORT).show()
         }
     }
+
+    private val BroadcastReceiver : BroadcastReceiver = object :BroadcastReceiver(){
+        override fun onReceive(p0: Context?, p1: Intent?) {
+            var actionColor = p1!!.getStringExtra("action")
+            when(actionColor!!){
+                "Blue" -> {
+                    selectedColor = p1.getStringExtra("selectedColor")!!
+                    colorView.setBackgroundColor(Color.parseColor(selectedColor))
+                }
+                "Yellow" -> {
+                    selectedColor = p1.getStringExtra("selectedColor")!!
+                    colorView.setBackgroundColor(Color.parseColor(selectedColor))
+                }
+                "Purple" -> {
+                    selectedColor = p1.getStringExtra("selectedColor")!!
+                    colorView.setBackgroundColor(Color.parseColor(selectedColor))
+                }
+                "Green" -> {
+                    selectedColor = p1.getStringExtra("selectedColor")!!
+                    colorView.setBackgroundColor(Color.parseColor(selectedColor))
+                }
+                "Orange" -> {
+                    selectedColor = p1.getStringExtra("selectedColor")!!
+                    colorView.setBackgroundColor(Color.parseColor(selectedColor))
+                }
+                "Black" -> {
+                    selectedColor = p1.getStringExtra("selectedColor")!!
+                    colorView.setBackgroundColor(Color.parseColor(selectedColor))
+                }
+                "Image" ->{
+                    readStorageTask()
+                    layoutWebUrl.visibility = View.GONE
+                }
+                "WebUrl" ->{
+                    layoutWebUrl.visibility = View.VISIBLE
+                }
+                "DeleteNote" -> {
+                    //delete note
+                    deleteNote()
+                }
+                else -> {
+                    layoutImage.visibility = View.GONE
+                    imgNote.visibility = View.GONE
+                    layoutWebUrl.visibility = View.GONE
+                    selectedColor = p1.getStringExtra("selectedColor")!!
+                    colorView.setBackgroundColor(Color.parseColor(selectedColor))
+                }
+            }
+        }
+    }
+    override fun onDestroy() {
+        LocalBroadcastManager.getInstance(requireContext()).unregisterReceiver(BroadcastReceiver)
+        super.onDestroy()
+    }
+    private fun hasReadStoragePerm():Boolean{
+        return EasyPermissions.hasPermissions(requireContext(),Manifest.permission.READ_EXTERNAL_STORAGE)
+    }
+    private fun readStorageTask(){
+        if (hasReadStoragePerm()){
+            pickImageFromGallery()
+        }else{
+            EasyPermissions.requestPermissions(
+                requireActivity(),
+                getString(R.string.storage_permission_text),
+                READ_STORAGE_PERM,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            )
+        }
+    }
+    private fun pickImageFromGallery(){
+        var intent = Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        if (intent.resolveActivity(requireActivity().packageManager) != null){
+            startActivityForResult(intent,REQUEST_CODE_IMAGE)
+        }
+    }
+    private fun getPathFromUri(contentUri: Uri): String? {
+        var filePath:String? = null
+        var cursor = requireActivity().contentResolver.query(contentUri,null,null,null,null)
+        if (cursor == null){
+            filePath = contentUri.path
+        }else{
+            cursor.moveToFirst()
+            var index = cursor.getColumnIndex("_data")
+            filePath = cursor.getString(index)
+            cursor.close()
+        }
+        return filePath
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_CODE_IMAGE && resultCode == RESULT_OK){
+            if (data != null){
+                var selectedImageUrl = data.data
+                if (selectedImageUrl != null){
+                    try {
+                        var inputStream = requireActivity().contentResolver.openInputStream(selectedImageUrl)
+                        var bitmap = BitmapFactory.decodeStream(inputStream)
+                        imgNote.setImageBitmap(bitmap)
+                        imgNote.visibility = View.VISIBLE
+                        layoutImage.visibility = View.VISIBLE
+                        selectedImagePath = getPathFromUri(selectedImageUrl)!!
+                    }catch (e:Exception){
+                        Toast.makeText(requireContext(),e.message,Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
+    }
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        EasyPermissions.onRequestPermissionsResult(requestCode,permissions,grantResults,requireActivity())
+    }
+
+    override fun onPermissionsDenied(requestCode: Int, perms: MutableList<String>) {
+        if (EasyPermissions.somePermissionPermanentlyDenied(requireActivity(),perms)){
+            AppSettingsDialog.Builder(requireActivity()).build().show()
+        }
+    }
+    override fun onPermissionsGranted(requestCode: Int, perms: MutableList<String>) {
+    }
+    override fun onRationaleDenied(requestCode: Int) {
+    }
+    override fun onRationaleAccepted(requestCode: Int) {
+    }
+}
